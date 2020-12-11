366f063914754f458bc02272772a00a8e4ac5add
Jonathan Ho <JonathanHo@quickenloans.com>
2020-12-11 17:05:28 -0500

    Fixes GetLetterGrade bug

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,13 +19,13 @@ namespace GradeBook.GradeBooks
             var studentsPerGrade = (int)Math.Ceiling(0.2 * Students.Count);
             var averageGrades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
 
-            if (averageGrade <= averageGrades[studentsPerGrade - 1])
+            if (averageGrade >= averageGrades[studentsPerGrade - 1])
                 return 'A';
-            if (averageGrade <= averageGrades[studentsPerGrade * 2 - 1])
+            if (averageGrade >= averageGrades[studentsPerGrade * 2 - 1])
                 return 'B';
-            if (averageGrade <= averageGrades[studentsPerGrade * 3 - 1])
+            if (averageGrade >= averageGrades[studentsPerGrade * 3 - 1])
                 return 'C';
-            if (averageGrade <= averageGrades[studentsPerGrade * 4 - 1])
+            if (averageGrade >= averageGrades[studentsPerGrade * 4 - 1])
                 return 'D';
 
 
